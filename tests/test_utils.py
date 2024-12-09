9d5923142429c1340da5531177e90487216dac5c
Stella Douka <styliani.douka@inria.fr>
2024-12-09 11:58:17 +0100

    [test] update unit tests for utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -16,10 +16,14 @@ class TestUtils(unittest.TestCase):
         assert global_device() == torch.device("cuda")
         set_device("cpu")
         assert global_device() == torch.device("cpu")
+        set_device(torch.device("cuda"))
+        assert global_device() == torch.device("cuda")
+        set_device(torch.device("cpu"))
+        assert global_device() == torch.device("cpu")
 
     def test_torch_zeros(self) -> None:
         size = (random.randint(1, 10), random.randint(1, 10))
-        tensor = torch_zeros(*size)
+        tensor = torch_zeros(size)
         tensor_device = torch.device("cuda" if tensor.is_cuda else "cpu")
         assert tensor_device == global_device()
         assert tensor.shape == size
@@ -27,8 +31,67 @@ class TestUtils(unittest.TestCase):
 
     def test_torch_ones(self) -> None:
         size = (random.randint(1, 10), random.randint(1, 10))
-        tensor = torch_ones(*size)
+        tensor = torch_ones(size)
         tensor_device = torch.device("cuda" if tensor.is_cuda else "cpu")
         assert tensor_device == global_device()
         assert tensor.shape == size
         assert torch.all(tensor == 1)
+
+    def test_activation_fn(self) -> None:
+        assert isinstance(activation_fn(None), nn.Identity)
+        assert isinstance(activation_fn("Id"), nn.Identity)
+        assert isinstance(activation_fn("Test"), nn.Identity)
+        assert isinstance(activation_fn("Softmax"), nn.Softmax)
+        assert isinstance(activation_fn("SELU"), nn.SELU)
+        assert isinstance(activation_fn("RELU"), nn.ReLU)
+
+    def test_mini_batch_gradient_descent(self) -> None:
+        callable_forward = lambda x: x**2 + 1
+        cost_fn = lambda pred, y: torch.sum((pred - y) ** 2)
+        x = torch.rand((5, 2), requires_grad=True, device=global_device())
+        y = torch.rand((5, 1), device=global_device())
+        lrate = 1e-3
+        epochs = 50
+        batch_size = 8
+        with self.assertRaises(AttributeError):
+            mini_batch_gradient_descent(
+                callable_forward, cost_fn, x, y, lrate, epochs, batch_size, verbose=False
+            )
+            mini_batch_gradient_descent(
+                callable_forward,
+                cost_fn,
+                x,
+                y,
+                lrate,
+                epochs,
+                batch_size,
+                parameters=[],
+                verbose=False,
+            )
+
+        parameters = [x]
+        mini_batch_gradient_descent(
+            callable_forward,
+            cost_fn,
+            x,
+            y,
+            lrate,
+            epochs,
+            batch_size,
+            parameters,
+            verbose=False,
+        )
+
+        model = nn.Linear(2, 1, device=global_device())
+        eval_fn = lambda: None
+        mini_batch_gradient_descent(
+            model,
+            cost_fn,
+            x,
+            y,
+            lrate,
+            epochs,
+            batch_size,
+            eval_fn=eval_fn,
+            verbose=False,
+        )
