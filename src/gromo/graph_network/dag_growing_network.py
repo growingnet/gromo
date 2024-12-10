[fix] bug in f1 score calculation

src/gromo/graph_network/dag_growing_network.py
@@ -1508,7 +1508,7 @@ class GraphGrowingNetwork(torch.nn.Module):
 
         if with_f1score:
             if self.out_features > 1:
-                f1score = f1_micro(y, final_pred)
+                f1score = f1_micro(y.cpu(), final_pred.cpu())
             else:
                 f1score = -1
             return accuracy, loss.item(), f1score

src/gromo/utils/utils.py
@@ -465,9 +465,9 @@ def calculate_true_positives(
     tuple[float, float, float]
         true positives, false positives, false negatives
     """
-    true_positives = np.sum((actual == label) & (predicted == label))
-    false_positives = np.sum((actual != label) & (predicted == label))
-    false_negatives = np.sum((predicted != label) & (actual == label))
+    true_positives = torch.sum((actual == label) & (predicted == label)).item()
+    false_positives = torch.sum((actual != label) & (predicted == label)).item()
+    false_negatives = torch.sum((predicted != label) & (actual == label)).item()
 
     return true_positives, false_positives, false_negatives
 