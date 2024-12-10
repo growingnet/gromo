[fix] pass batch normalization as a function

src/gromo/graph_network/GrowableDAG.py
@@ -314,15 +314,22 @@ class GrowableDAG(nx.DiGraph, nn.Module):
                 )
             self.nodes[node].update(attributes)
             if self.nodes[node]["type"] == "L":
+                in_features = self.nodes[node]["size"]
+                if attributes.get("use_batch_norm", False):
+                    batch_norm = nn.BatchNorm1d(
+                        in_features, affine=False, device=self.device
+                    )
+                else:
+                    batch_norm = nn.Identity()
                 self.__set_node_module(
                     node,
                     LinearAdditionGrowingModule(
                         allow_growing=True,
-                        in_features=self.nodes[node]["size"],
+                        in_features=in_features,
                         post_addition_function=activation_fn(
                             self.nodes[node].get("activation")
                         ),
-                        batch_norm=attributes.get("use_batch_norm", False),
+                        post_addition_normalization=batch_norm,
                         device=self.device,
                         name=f"{node}",
                     ),

src/gromo/growing_module.py
@@ -17,7 +17,7 @@ class AdditionGrowingModule(torch.nn.Module):
     def __init__(
         self,
         post_addition_function: torch.nn.Module = torch.nn.Identity(),
-        batch_norm: bool = False,
+        post_addition_normalization: torch.nn.Module = torch.nn.Identity(),
         previous_modules: list["AdditionGrowingModule | GrowingModule"] = None,
         next_modules: list["AdditionGrowingModule | GrowingModule"] = None,
         allow_growing: bool = False,
@@ -38,10 +38,12 @@ class AdditionGrowingModule(torch.nn.Module):
             device if device else global_device()
         )  # FIXME: this could be removed
 
-        self.batch_norm = batch_norm
         self.post_addition_function: torch.nn.Module = post_addition_function
         if self.post_addition_function:
             self.post_addition_function = self.post_addition_function.to(self.device)
+        self.post_addition_normalization: torch.nn.Module = post_addition_normalization
+        if self.post_addition_normalization:
+            self.post_addition_normalization = self.post_addition_normalization.to(device)
         self._allow_growing = allow_growing
 
         self.store_input = 0
@@ -177,8 +179,8 @@ class AdditionGrowingModule(torch.nn.Module):
             self.input.retain_grad()
 
         if x is not None:
-            if self.batch_norm:
-                x = torch.nn.BatchNorm1d(x.shape[1], affine=False, device=self.device)(x)
+            if self.post_addition_normalization:
+                x = self.post_addition_normalization(x)
 
             if self.post_addition_function:
                 y = self.post_addition_function(x)