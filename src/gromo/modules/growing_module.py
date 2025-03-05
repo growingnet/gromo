Fix compute_optimal_updates when zero_delta=True. The computation of the optimal added neurons of a growing module uses self.delta_raw which is not set to zero if zero_delta=True in the current implementation. This should be included in the tests but might be tricky to do so.

src/gromo/modules/growing_module.py
@@ -1267,7 +1267,7 @@ class GrowingModule(torch.nn.Module):
         update_previous: bool
             whether to change the previous layer extended_output_layer
         zero_delta: bool
-            if True, set the optimal delta to zero
+            if True, compute the optimal added neurons without performing the natural gradient step.
         dtype: torch.dtype
             dtype for the computation of the optimal delta and added parameters
 