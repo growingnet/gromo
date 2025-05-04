3e3bfd81930fbea0e9234c7073baa199efbe7e80
Théo Rudkiewicz <theo.rudkiewicz@inria.fr>
2025-05-04 21:49:53 +0200

    enh: Make the tutorial compatible with the sphinx builder

--- examples/plot_tutorial.py
+++ examples/plot_tutorial.py
@@ -1,26 +1,31 @@
-#!/usr/bin/env python
-# coding: utf-8
+"""
+GroMo tutorial
+==============
 
-# # GroMo tutorial
-#
-#
-# This is a minimal example of how to use GroMo. We will illustrate the use off GroMo to find a one hidden layer neural network (NN) that approximates the function $f(x) = \sin(x)$ on the interval $[0, 2\pi]$.
-
-# ## Imports
-#
-# - We use `torch` as the backend for all the computations.
-# - `matplotlib` is used for plotting the results.
-# - `global_device` is used to automatically select the device (CPU or GPU) for computations.
-# - `LinearGrowingModule` is the main class of GroMo, which implements fully connected growing modules.
-# - `SinDataLoader` is a custom data loader that generates the training data for the sine function.
-# - `train` is exactly like a **standard PyTorch training loop**
-# - `evaluate` is exactly like a **standard PyTorch evaluation loop**
-#
-# Then we define `plt_model` to visualize the model.
-
-# In[1]:
+This is a minimal example of how to use GroMo. We will illustrate the
+use off GroMo to find a one hidden layer neural network (NN) that
+approximates the function :math:`f(x) = \sin(x)` on the interval
+:math:`[0, 2\pi]`.
 
 
+Imports
+-------
+
+-  We use ``torch`` as the backend for all the computations.
+-  ``matplotlib`` is used for plotting the results.
+-  ``global_device`` is used to automatically select the device (CPU or
+   GPU) for computations.
+-  ``LinearGrowingModule`` is the main class of GroMo, which implements
+   fully connected growing modules.
+-  ``SinDataLoader`` is a custom data loader that generates the training
+   data for the sine function.
+-  ``train`` is exactly like a **standard PyTorch training loop**
+-  ``evaluate`` is exactly like a **standard PyTorch evaluation loop**
+
+Then we define ``plt_model`` to visualize the model.
+"""
+
+###############################################################################
 import matplotlib.pyplot as plt
 import torch
 from helpers.auxilliary_functions import SinDataloader, evaluate_model, train
@@ -31,8 +36,7 @@ from gromo.utils.utils import global_device
 
 global_device()
 
-
-# In[2]:
+###############################################################################
 
 
 def plt_model(model: torch.nn.Module, fig: "plt.axes._axes.Axes") -> None:
@@ -61,17 +65,13 @@ def plt_model(model: torch.nn.Module, fig: "plt.axes._axes.Axes") -> None:
     fig.set_ylabel("sin(x)")
 
 
-# In[3]:
-
+###############################################################################
 
 data = SinDataloader(nb_sample=10, batch_size=100)
 loss_function = torch.nn.MSELoss()
-
-
-# ## 1. Define the model
-
-# In[4]:
-
+###############################################################################
+# 1. Define the model
+# -------------------
 
 first_layer = LinearGrowingModule(
     in_features=1,
@@ -97,32 +97,33 @@ growing_net = torch.nn.Sequential(
 growing_net = growing_net.to(global_device())
 
 print(growing_net)
-
-
+###############################################################################
 # Here we define the following network:
 #
-# $$ x \mapsto \begin{bmatrix} z_1 \\ z_2 \end{bmatrix} \mapsto \begin{bmatrix} \sigma(z_1) \\ \sigma(z_2) \end{bmatrix} \mapsto y $$
+# .. math::  x \mapsto \begin{bmatrix} z_1 \\ z_2 \end{bmatrix} \mapsto \begin{bmatrix} \sigma(z_1) \\ \sigma(z_2) \end{bmatrix} \mapsto y
 #
-# where $\sigma$ is the activation function, $z_1$ and $z_2$ are the outputs of the first fully connected layer, and $y$ is the output of the whole network.
+# where :math:`\sigma` is the activation function, :math:`z_1` and
+# :math:`z_2` are the outputs of the first fully connected layer, and
+# :math:`y` is the output of the whole network.
 #
-# Note that the **activation function is included in the first `LinearGrowingModule` layer**, this allow to easily access the intermediate results both before and after the activation function.
+# Note that the **activation function is included in the first
+# ``LinearGrowingModule`` layer**, this allow to easily access the
+# intermediate results both before and after the activation function.
 #
-# Not also that the second layer is is linked to the first one by `previous_module=first_layer`. This allow to extend the input of the second layer and let it grow with the first layer.
-
-# ### 2. Use it like a normal model
-
-# In[5]:
-
+# Not also that the second layer is is linked to the first one by
+# ``previous_module=first_layer``. This allow to extend the input of the
+# second layer and let it grow with the first layer.
+#
+# 2. Use it like a normal model
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 l2_err = evaluate_model(growing_net, data, loss_function)[0]
 print(f"Initial error: {l2_err:.2e}")
 fig, ax = plt.subplots(1, 1, figsize=(10, 5))
 plt_model(growing_net, ax)
 
-
-# In[6]:
-
-
+###############################################################################
+#
 # Here we guide it a bit in the right direction to make it learn faster
 
 growing_net[0].weight.data = torch.ones_like(growing_net[0].weight.data)
@@ -130,18 +131,14 @@ growing_net[0].bias.data = torch.tensor([-2.0, -3 * torch.pi / 2], device=global
 growing_net[1].weight.data = torch.tensor([[-1.0, 2.0]], device=global_device())
 growing_net[1].bias.data = torch.zeros_like(growing_net[1].bias.data)
 
-
-# In[7]:
-
+###############################################################################
 
 l2_err = evaluate_model(growing_net, data, loss_function)[0]
 print(f"Initial error: {l2_err:.2e}")
 fig, ax = plt.subplots(1, 1, figsize=(10, 5))
 plt_model(growing_net, ax)
 
-
-# In[8]:
-
+###############################################################################
 
 optimizer = torch.optim.SGD(growing_net.parameters(), lr=1e-2)
 # optimizer = torch.optim.Adam(net.parameters(), lr=1e-3)
@@ -164,42 +161,39 @@ l2_err = evaluate_model(growing_net, data, loss_function, aux_loss_function=None
 print(f"Initial error: {l2_err:.2e}")
 fig, ax = plt.subplots(1, 1, figsize=(10, 5))
 plt_model(growing_net, ax)
-
-
-# Here with only two hidden neurons, we have a limited expressiveness. Therefore we would like to add new neurons to the model.
-
-# ## 3. Prepare the growth
+###############################################################################
+# Here with only two hidden neurons, we have a limited expressiveness.
+# Therefore we would like to add new neurons to the model.
 #
-# To add new neurons we need information about the current model. To get those the first set is to initialize the computation of those.
-
-# In[9]:
-
+# 3. Prepare the growth
+# ---------------------
+#
+# To add new neurons we need information about the current model. To get
+# those the first set is to initialize the computation of those.
+#
 
 print(growing_net[0].__str__(verbose=2))
 print(growing_net[1].__str__(verbose=2))
-
-
+###############################################################################
 # Above you can see that nothing is stored in the model.
-
-# In[10]:
-
+#
 
 growing_net[1].init_computation()
 
-
-# In[11]:
-
+###############################################################################
 
 print(growing_net[0].__str__(verbose=2))
 print(growing_net[1].__str__(verbose=2))
-
-
-# Above you can see that one the computation are initialised, we see that `Store input : True`. This means that the next time we forward through the graph we will store the input of the layers for which `store_input=True`.
+###############################################################################
+# Above you can see that one the computation are initialised, we see that
+# ``Store input : True``. This means that the next time we forward through
+# the graph we will store the input of the layers for which
+# ``store_input=True``.
 #
-# We then do the forward/backward pass to compute all the raw information needed, then we call `update_computation` to aggregate the raw informatons into statistics like the tensors S and M.
-
-# In[12]:
-
+# We then do the forward/backward pass to compute all the raw information
+# needed, then we call ``update_computation`` to aggregate the raw
+# informatons into statistics like the tensors S and M.
+#
 
 # Here we switch to a sum loss function !
 # This is important as we already make the average internally
@@ -213,58 +207,55 @@ for x, sinx in data:
 
     growing_net[1].update_computation()
 
-
-# Below you can see that indeed `Tensor S` and `Tensor M` are now estimated over 1000 samples.
-
-# In[13]:
-
+###############################################################################
+# Below you can see that indeed ``Tensor S`` and ``Tensor M`` are now
+# estimated over 1000 samples.
+#
 
 print(growing_net[0].__str__(verbose=2))
 print(growing_net[1].__str__(verbose=2))
-
-
-# Then we can compute the natural gradient step and the new neurons to add with `compute_optimal_updates`. You can see that now the first layer store an extended output layer that will compute the new values of the neurons. The second layer has also been extended with an extended input layer. In addition the second layer has a `Optimal delta layer` which correspond to the natural gradient step.
-
-# In[14]:
-
+###############################################################################
+# Then we can compute the natural gradient step and the new neurons to add
+# with ``compute_optimal_updates``. You can see that now the first layer
+# store an extended output layer that will compute the new values of the
+# neurons. The second layer has also been extended with an extended input
+# layer. In addition the second layer has a ``Optimal delta layer`` which
+# correspond to the natural gradient step.
+#
 
 growing_net[1].compute_optimal_updates()
 print(growing_net[0].__str__(verbose=2))
 print(growing_net[1].__str__(verbose=2))
-
-
-# Once the updates are computed we can stop computing statistics and storing them. This is done by calling the `reset_computation` method.
-
-# In[15]:
-
+###############################################################################
+# Once the updates are computed we can stop computing statistics and
+# storing them. This is done by calling the ``reset_computation`` method.
+#
 
 growing_net[1].reset_computation()
 print(growing_net[0].__str__(verbose=2))
 print(growing_net[1].__str__(verbose=2))
-
-
-# Here we can see that the first layer still store the input. To correct it we can simply set `store_input=False` in the first layer.
-
-# In[16]:
-
+###############################################################################
+# Here we can see that the first layer still store the input. To correct
+# it we can simply set ``store_input=False`` in the first layer.
+#
 
 growing_net[0].store_input = False
 print(growing_net[0].__str__(verbose=2))
 print(growing_net[1].__str__(verbose=2))
-
-
-# ## 4. Choose a scaling factor
+###############################################################################
+# 4. Choose a scaling factor
+# --------------------------
 #
-# Once we computed the updates we can choose a scaling factor. This scaling factor $\gamma$ will scale the updates by $\gamma$ when they have a quadratic effect (like the new incoming and outgoing weights) or $\gamma^2$ when they have a linear effect (like the natural gradient step).
+# Once we computed the updates we can choose a scaling factor. This
+# scaling factor :math:`\gamma` will scale the updates by :math:`\gamma`
+# when they have a quadratic effect (like the new incoming and outgoing
+# weights) or :math:`\gamma^2` when they have a linear effect (like the
+# natural gradient step).
 #
-
-# In[17]:
-
 
 growing_net[1].scaling_factor = 0.5
 
-
-# In[18]:
+###############################################################################
 
 
 def extended_evaluate_model(
@@ -295,10 +286,11 @@ def extended_evaluate_model(
     return total_loss.item() / nb_sample
 
 
-# We can use a special forward `extended_forward` that takes into account the proposed modification of the network to evaluate their effect on the loss.
-
-# In[19]:
-
+###############################################################################
+# We can use a special forward ``extended_forward`` that takes into
+# account the proposed modification of the network to evaluate their
+# effect on the loss.
+#
 
 new_error = extended_evaluate_model(
     growing_net,
@@ -307,45 +299,38 @@ new_error = extended_evaluate_model(
 )
 
 print(f"New error: {new_error:.2e}")
-
-
-# ### 5. Apply the changes
+###############################################################################
+# 5. Apply the changes
+# ~~~~~~~~~~~~~~~~~~~~
 #
-# Once we have chosen a scaling factor we can apply the changes to the model. This is done by calling the `apply_change` methods.
-
-# In[20]:
-
+# Once we have chosen a scaling factor we can apply the changes to the
+# model. This is done by calling the ``apply_change`` methods.
 
 growing_net[1].apply_change()
 print(growing_net[0].__str__(verbose=2))
 print(growing_net[1].__str__(verbose=2))
-
-
-# We can then delete the `extended_output_layer`, `extended_input_layer` and `Optimal delta layer` as they are not needed anymore.
-
-# In[21]:
-
+###############################################################################
+# We can then delete the ``extended_output_layer``,
+# ``extended_input_layer`` and ``Optimal delta layer`` as they are not
+# needed anymore.
 
 growing_net[1].delete_update()
 print(growing_net[0].__str__(verbose=2))
 print(growing_net[1].__str__(verbose=2))
-
-
-# ### 6. Use your grown model
+###############################################################################
+# 6. Use your grown model
+# ~~~~~~~~~~~~~~~~~~~~~~~
 #
-# You then get a fully working model that can be used like a normal PyTorch model. You can train it, evaluate it, etc.
-
-# In[22]:
-
+# You then get a fully working model that can be used like a normal
+# PyTorch model. You can train it, evaluate it, etc.
+#
 
 l2_err = evaluate_model(growing_net, data, loss_function)[0]
 print(f"Initial error: {l2_err:.2e}")
 fig, ax = plt.subplots(1, 1, figsize=(10, 5))
 plt_model(growing_net, ax)
 
-
-# In[23]:
-
-
+###############################################################################
+#
 print(growing_net[0].__str__(verbose=2))
 print(growing_net[1].__str__(verbose=2))
