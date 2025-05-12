f65b894995fcfd3bc95e66ac0990cf617eeb912a
Leo Burgund <leo.burgund@gmail.com>
2025-05-13 00:58:26 +0200

    feat(attention): simple transformer block and exemple with gamma forward

---
+++ src/gromo/modules/attention/transf_baseline.py
@@ -0,0 +1,86 @@
+import math
+from dataclasses import dataclass
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+from torch.nn import LayerNorm
+
+
+@dataclass
+class ModelConfig:
+    d_s: int = 4
+    d_e: int = 16
+    d_k: int = 8
+    d_v: int = 8
+    bias: bool = False
+
+
+class SelfAttentionBaseline(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.W_Q = nn.Linear(cfg.d_e, cfg.d_k, bias=cfg.bias)
+        self.W_K = nn.Linear(cfg.d_e, cfg.d_k, bias=cfg.bias)
+        self.W_V = nn.Linear(cfg.d_e, cfg.d_v, bias=cfg.bias)
+        self.W_O = nn.Linear(cfg.d_v, cfg.d_e, bias=cfg.bias)
+        self.scale = math.sqrt(cfg.d_k)
+        self.S_grad = None
+
+    def save_S_grad(self, grad: torch.Tensor) -> None:
+        """Hook to save the gradient of S."""
+        self.S_grad = grad
+
+    def get_S_grad(self) -> torch.Tensor:
+        """Return the gradient of S from the last backward pass"""
+        assert self.S_grad is not None, (
+            "S_grad is not available. Make sure to call forward() first."
+        )
+        return self.S_grad
+
+    def forward(self, X, gamma: None | float = None):
+        """If gamma is not None, compute the forward using (S + gamma * S_grad) instead of S"""
+        Q = self.W_Q(X)  # Compute query vectors
+        K = self.W_K(X)  # Compute key vectors
+        V = self.W_V(X)  # Compute value vectors
+
+        S = (Q @ K.transpose(-2, -1)) * (1 / self.scale)
+
+        # We save the gradient of S
+        if S.requires_grad:
+            S.register_hook(self.save_S_grad)
+
+        if gamma is not None:
+            assert self.S_grad is not None
+            S += gamma * self.S_grad
+
+        A = F.softmax(S, dim=-1)  # Apply softmax to get attention weights
+        H = A @ V
+        y = self.W_O(H)
+        return y
+
+
+class MLP(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.lin1 = nn.Linear(cfg.d_e, 4 * cfg.d_e, bias=cfg.bias)
+        self.gelu = nn.GELU()
+        self.lin2 = nn.Linear(4 * cfg.d_e, cfg.d_e, bias=cfg.bias)
+
+    def forward(self, x):
+        x = self.lin1(x)
+        x = self.gelu(x)
+        x = self.lin2(x)
+        return x
+
+
+class Block(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.ln1 = LayerNorm(cfg.d_e, eps=1e-5, bias=cfg.bias)
+        self.attn = SelfAttentionBaseline(cfg)
+        self.ln2 = LayerNorm(cfg.d_e, eps=1e-5, bias=cfg.bias)
+        self.mlp = MLP(cfg)
+
+    def forward(self, x, gamma: None | float = None):
+        x = x + self.attn(self.ln1(x), gamma)
+        x = x + self.mlp(self.ln2(x))
+        return x
---
+++ src/gromo/modules/attention/transf_ex.py
@@ -0,0 +1,139 @@
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader, Dataset, random_split
+
+from gromo.modules.attention.transf_baseline import Block, ModelConfig
+from gromo.utils.utils import global_device
+
+
+def generate_teacher_dataset(cfg, path, device, N_samples=5000, gen_batch=128):
+    # Create and freeze the teacher model
+    teacher = Block(cfg).to(device)
+    teacher.eval()
+    for z in teacher.parameters():
+        z.requires_grad = False
+
+    # Generate the dataset
+    all_X, all_Y = [], []
+    with torch.no_grad():
+        for _ in range(0, N_samples, gen_batch):
+            Xb = torch.randn(gen_batch, cfg.d_s, cfg.d_e, device=device)
+            Yb = teacher.forward(Xb)
+            all_X.append(Xb.cpu())
+            all_Y.append(Yb.cpu())
+
+    X = torch.cat(all_X, dim=0)
+    Y = torch.cat(all_Y, dim=0)
+    torch.save({"X": X, "Y": Y}, path)
+
+    print(f"Saved dataset with {X.size(0)} samples to {path}")
+
+
+class AttentionDataset(Dataset):
+    def __init__(self, path):
+        data = torch.load(path)
+        self.X, self.Y = data["X"], data["Y"]
+
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self, idx):
+        return self.X[idx], self.Y[idx]
+
+
+if __name__ == "__main__":
+    torch.manual_seed(0)
+    device = global_device()
+    print(f"Device:{device}")
+
+    DATA_PATH = "src/gromo/modules/attention/transf_teacher.pt"
+    test_ratio = 0.2
+    train_batch = 64
+    lr = 1e-3
+    num_epochs = 1000
+    gammas = list(np.linspace(0, 10000, 10))
+    # gammas = range(-10000, 10000, 1000)
+
+    config = ModelConfig(
+        d_s=4,
+        d_e=16,
+        d_k=8,
+        d_v=8,
+        bias=False,
+    )
+
+    if not os.path.exists(DATA_PATH):
+        generate_teacher_dataset(config, DATA_PATH, device)
+    assert os.path.exists(DATA_PATH), f"Dataset not found at {DATA_PATH}."
+
+    dataset = AttentionDataset(DATA_PATH)
+    test_size = int(test_ratio * len(dataset))
+    train_size = len(dataset) - test_size
+    train_ds, test_ds = random_split(dataset, [train_size, test_size])
+
+    train_loader = DataLoader(train_ds, batch_size=train_batch, shuffle=True)
+    test_loader = DataLoader(test_ds, batch_size=train_batch)
+
+    model = Block(config).to(device)
+
+    optimizer = torch.optim.SGD(model.parameters(), lr=lr)
+
+    loss_fn = nn.MSELoss()
+    train_losses = []
+    gammas_test_losses = {}
+    for gamma in gammas:
+        gammas_test_losses[gamma] = []
+
+    for epoch in range(1, num_epochs + 1):
+        model.train()
+        running_loss = 0.0
+        for xb, yb in train_loader:
+            xb, yb = xb.to(device), yb.to(device)
+            optimizer.zero_grad()
+            y_pred = model(xb)
+            loss = loss_fn(y_pred, yb)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item() * xb.size(0)
+        epoch_train_loss = running_loss / train_size
+        train_losses.append(epoch_train_loss)
+
+        model.eval()
+
+        for gamma in gammas:
+            assert gamma is not None
+            running_test = 0.0
+            with torch.no_grad():
+                for xb, yb in test_loader:
+                    xb, yb = xb.to(device), yb.to(device)
+                    y_pred = model.forward(xb, gamma=gamma)
+                    running_test += loss_fn(y_pred, yb).item() * xb.size(0)
+
+            epoch_test_loss = running_test / test_size
+            gammas_test_losses[gamma].append(epoch_test_loss)
+
+        if (epoch == 1) or (epoch % 100 == 0):
+            print(
+                f"Epoch {epoch}/{num_epochs} — Train Loss: {epoch_train_loss:.6f}, "
+                # f"Baseline Test Loss: {epoch_test_loss:.6f}"
+            )
+            for gamma in gammas:
+                print(f"Test Loss (gamma={gamma}): {gammas_test_losses[gamma][-1]:.6f}")
+
+    if True:
+        plt.figure()
+        plt.plot(range(1, num_epochs + 1), train_losses)
+        legend = ["Train"]
+        for gamma in gammas:
+            plt.plot(range(1, num_epochs + 1), gammas_test_losses[gamma])
+            legend.append("gamma=" + str(gamma))
+        plt.xlabel("Epoch")
+        plt.ylabel("Loss")
+        plt.yscale("log")
+        plt.legend(legend)
+        plt.title("Training vs. Testing Loss")
+        plt.show()
