fix/feat: Natural gradient now works. Implemented experiement for the SVD formula.
Test between the two possible SVD formulas:
- WQtplus1, WKtplus1 = SVD(WQt @ WKt.T - lbd * P)
- dWQ, dWK = SVD(- lbd * P)

src/gromo/modules/attention/ex_growing.py
@@ -1,7 +1,9 @@
 import os
+import matplotlib.pyplot as plt
 
 import torch
 import torch.nn as nn
+import numpy as np
 from torch.utils.data import DataLoader, random_split
 
 from gromo.modules.attention.model import Block, ModelConfig
@@ -18,7 +20,7 @@ DATA_PATH = "src/gromo/modules/attention/transf_teacher.pt"
 test_ratio = 0.2
 
 # Hyperparams training
-num_epochs = 10
+num_epochs = 4
 log_every_x_epochs = num_epochs // 10 if num_epochs > 10 else 1
 train_batch = 64
 lr = 1e-3
@@ -48,23 +50,26 @@ test_loader = DataLoader(test_ds, batch_size=train_batch)
 model = Block(config).to(device)
 optimizer = torch.optim.SGD(model.parameters(), lr=lr)
 loss_fn = nn.MSELoss()
+lbds = [i for i in np.linspace(0, 1e6, 16 + 1)]
 train_losses = []
+lbds_epoch_losses = []
+lbds_epoch_losses_dif = []
 for epoch in range(1, num_epochs + 1):
     model.train()
     running_loss = 0.0
     fro_mean_outside, op_mean_outside = 0.0, 0.0
     fro_mean_inside, op_mean_inside = 0.0, 0.0
+    running_lbds_train_losses = {lbd: 0.0 for lbd in lbds}
+    running_lbds_train_losses_dif = {lbd: 0.0 for lbd in lbds}
 
     for xb, yb in train_loader:
         xb, yb = xb.to(device), yb.to(device)
         optimizer.zero_grad()
-        y_pred = model.forward(xb)
+        y_pred = model.forward(xb)  # Retain the attention block input
         loss = loss_fn(y_pred, yb)
-        loss.backward()  # Get S_grad
-        # TODO: Check ordre algo, notamment calcul S_grad pour bien verif pas bug
+        loss.backward()  # Retain S_grad
 
-        growing = True
-        if epoch != 1:
+        if epoch % 2 == 0:
             with torch.no_grad():
                 model.compute_statistics(outside_esp=False)
                 fro_inside, op_inside = model.get_P_ratios()
@@ -76,11 +81,29 @@ for epoch in range(1, num_epochs + 1):
                 fro_mean_outside += fro_outside.item() * xb.size(0)
                 op_mean_outside += op_outside.item() * xb.size(0)
 
-                model.update_WQ_WK(config)
+                # lbd search
+                model.save_WQt_WKt()
+                for lbd in lbds:
+                    model.update_WQ_WK(config, lbd=lbd)
+                    y_pred_search = model.forward(xb)
+                    loss_search = loss_fn(y_pred_search, yb)
+                    running_lbds_train_losses[lbd] += loss_search.item() * xb.size(0)
+                for lbd in lbds:
+                    model.update_WQ_WK(config, lbd=lbd, dif=True)
+                    y_pred_search = model.forward(xb)
+                    loss_search = loss_fn(y_pred_search, yb)
+                    running_lbds_train_losses_dif[lbd] += loss_search.item() * xb.size(0)
+                model.reset_WQt_WKt(config)
+
         else:
             optimizer.step()
 
         running_loss += loss.item() * xb.size(0)
+
+    for lbd in lbds:
+        running_lbds_train_losses[lbd] /= train_size
+        running_lbds_train_losses_dif[lbd] /= train_size
+        print(f"lbd: {lbd}, Loss: {running_lbds_train_losses[lbd]}")
 
     epoch_loss = running_loss / train_size
     epoch_fro_outside = fro_mean_outside / train_size
@@ -91,47 +114,26 @@ for epoch in range(1, num_epochs + 1):
     print(f"Outside Fro: \t{epoch_fro_outside:.4f}, Op: \t{epoch_op_outside:.4f}")
     print(f"Inside Fro: \t{epoch_fro_inside:.4f}, Op: \t{epoch_op_inside:.4f}")
     train_losses.append(epoch_loss)
+    lbds_epoch_losses.append(running_lbds_train_losses)
+    lbds_epoch_losses_dif.append(running_lbds_train_losses_dif)
+
+plt.figure()
+legend = []
+for epoch in range(1, num_epochs + 1):
+    if epoch % 2 == 0:
+        plt.plot(lbds, [lbds_epoch_losses[epoch - 1][lbd] for lbd in lbds])
+        legend.append(f"Epoch {epoch}")
+        plt.plot(lbds, [lbds_epoch_losses_dif[epoch - 1][lbd] for lbd in lbds])
+        legend.append(f"Epoch {epoch} dif")
+plt.yscale("log")
+plt.xlabel("Lambda")
+plt.ylabel("Loss")
+plt.title("Train Loss (lambda)")
+plt.legend(legend)
+plt.show()
 
 
 # Evaluate on test set
-model.eval()
-test_loss = 0.0
-with torch.no_grad():
-    for xb, yb in test_loader:
-        xb, yb = xb.to(device), yb.to(device)
-        y_pred = model(xb)
-        loss = loss_fn(y_pred, yb)
-        test_loss += loss.item() * xb.size(0)
-test_loss /= test_size
-print(f"Test Loss: {test_loss:.4f}")
-
-
-# model = Block(config).to(device)
-# optimizer = torch.optim.SGD(model.parameters(), lr=lr)
-# loss_fn = nn.MSELoss()
-#
-# train_losses = []
-#
-# for epoch in range(1, num_epochs + 1):
-#     model.train()
-#     running_loss = 0.0
-#
-#     for xb, yb in train_loader:
-#         xb, yb = xb.to(device), yb.to(device)
-#         optimizer.zero_grad()
-#         y_pred = model(xb)
-#         loss = loss_fn(y_pred, yb)
-#         loss.backward()  # Get S_grad
-#         optimizer.step()
-#         running_loss += loss.item() * xb.size(0)
-#
-#     epoch_loss = running_loss / train_size
-#     train_losses.append(epoch_loss)
-#
-#     if epoch % log_every_x_epochs == 0 or epoch == 1 or epoch == num_epochs:
-#         print(f"Epoch {epoch}/{num_epochs}, Loss: {epoch_loss:.4f}")
-#
-# # Evaluate on test set
 # model.eval()
 # test_loss = 0.0
 # with torch.no_grad():

src/gromo/modules/attention/model.py
@@ -128,25 +128,58 @@ class Block(nn.Module):
         ) / torch.linalg.matrix_norm(self.P_leo, ord=2, dim=(-2, -1))
         return fro, op
 
-    def update_WQ_WK(self, cfg):
-        """Update the weights of W_Q and W_K using the natural gradient"""
-        assert self.P_leo is not None
-        self.WQ_copy = self.attn.W_Q.weight.clone().T
-        self.WK_copy = self.attn.W_K.weight.clone().T
+    def save_WQt_WKt(self):
+        # Notation (out,in) -> (in,out)
+        self.WQt = self.attn.W_Q.weight.clone().T
+        self.WKt = self.attn.W_K.weight.clone().T
 
-        lbd = 10000000  # TODO: Get this with line search
-        WQ_new, WK_new = my_svd_low_rank(
-            self.WQ_copy @ self.WK_copy.T - lbd * self.P_leo, cfg.d_k
-        )
-        WQ_new = WQ_new.T
-        WK_new = WK_new.T
+    def reset_WQt_WKt(self, cfg):
+        """
+        Restore the linear layers W_Q and W_K using the saved WQt and WKt.
+        """
+        WQ_layer = nn.Linear(cfg.d_e, cfg.d_k, bias=cfg.bias)
+        WK_layer = nn.Linear(cfg.d_e, cfg.d_k, bias=cfg.bias)
+        with torch.no_grad():
+            WQ_layer.weight.copy_(self.WQt.T)
+            WK_layer.weight.copy_(self.WKt.T)
+        self.attn.W_Q = WQ_layer
+        self.attn.W_K = WK_layer
+
+    def update_WQ_WK(self, cfg, lbd: float, dif: bool = False, verbose: bool = False):
+        """
+        Update the linear layers W_Q and W_K.
+        The update depends on the saved WQt and WKt, lbd, and the statistic P.
+
+        dif: If True, find dWQ, dWK = SVD(-lbd * P); instead of finding directly WQ, WK
+        """
+        assert self.P_leo is not None
 
         new_WQ = nn.Linear(cfg.d_e, cfg.d_k, bias=cfg.bias)
         new_WK = nn.Linear(cfg.d_e, cfg.d_k, bias=cfg.bias)
 
-        with torch.no_grad():
-            new_WQ.weight.copy_(WQ_new)
-            new_WK.weight.copy_(WK_new)
+        if not dif:
+            WQtplus1, WKtplus1 = my_svd_low_rank(
+                self.WQt @ self.WKt.T - lbd * self.P_leo, cfg.d_k
+            )
 
-        self.W_Q = new_WQ
-        self.W_K = new_WK
+            # Notation (in, out) -> (out, in)
+            WQtplus1 = WQtplus1.T
+            WKtplus1 = WKtplus1.T
+        else:
+            dWQ, dWK = my_svd_low_rank(-lbd * self.P_leo, cfg.d_k)
+
+            # Notation (in, out) -> (out, in)
+            WQtplus1 = (self.WQt + dWQ).T
+            WKtplus1 = (self.WKt + dWK).T
+
+        with torch.no_grad():
+            new_WQ.weight.copy_(WQtplus1)
+            new_WK.weight.copy_(WKtplus1)
+
+        if verbose:
+            print(
+                f"Norm ratio WQ new/old: {torch.linalg.matrix_norm(new_WQ.weight, ord='fro') / torch.linalg.matrix_norm(self.attn.W_Q.weight, ord='fro')}"
+            )
+
+        self.attn.W_Q = new_WQ
+        self.attn.W_K = new_WK