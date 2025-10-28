58df8706d4f2a89f36eb7052167aab5fe692aba9
Théo Rudkiewicz <theo.rudkiewicz@inria.fr>
2025-10-28 14:28:13 +0100

    test(`TestTensorStatiticWithEstimationError`): Reach 100% covergae of |`tensor_statistic.py`

--- tests/test_tensor_statistics.py
+++ tests/test_tensor_statistics.py
@@ -17,7 +17,7 @@ class TestTensorStatistic(TestCase):
         set_device("cpu")
         x = None
         n_samples = 0
-        f = lambda: (x.sum(dim=0), x.size(0))  # type: ignore
+        f = lambda: (x.sum(dim=0), x.size(0))  # type: ignore  # noqa: E731
         tensor_statistic = self._tested_class(
             shape=(2, 3), update_function=f, name="Average"
         )
@@ -58,21 +58,22 @@ class TestTensorStatistic(TestCase):
         reset_device()
 
 
-class TestTensorStatisticWithError(TestTensorStatistic):
+class TestTensorStatiticWithEstimationError(TestTensorStatistic):
     _tested_class = TensorStatiticWithEstimationError
 
-    def test_error(self):
+    def setUp(self) -> None:
         set_device("cuda" if torch.cuda.is_available() else "cpu")
+        torch.manual_seed(0)
 
+    def test_error(self):
         num_batches = 10
         batch_size = 10
-        total_samples = num_batches * batch_size
+        total_samples = torch.Size((num_batches * batch_size,))
         mean = torch.tensor([3.0, 4.0])
         cov = torch.tensor([[1.0, 0.5], [0.5, 2.0]])
 
-        torch.manual_seed(42)
         dist = torch.distributions.MultivariateNormal(mean, covariance_matrix=cov)
-        samples = dist.sample((total_samples,))
+        samples = dist.sample(total_samples)
         dataloader = DataLoader(
             TensorDataset(samples), batch_size=batch_size, shuffle=False
         )
@@ -84,18 +85,17 @@ class TestTensorStatisticWithError(TestTensorStatistic):
         )
 
         self.assertRaises(AssertionError, mean_statistic.error)
-        i = 0
-        for (batch,) in dataloader:
-            i += 1
+        for i, (batch,) in enumerate(dataloader):
             mean_statistic.updated = False
             mean_statistic.update(x=batch)
-            if i == 1:
+            if i == 0:
                 self.assertEqual(mean_statistic.error(), float("inf"))
         self.assertEqual(mean_statistic.samples, num_batches * batch_size)
         true_error = torch.norm(mean_statistic() - mean).item() ** 2
         self.assertLessEqual(
             true_error, mean_statistic.error() * 3
-        )  # this test pass most of the time, but can fail due to randomness (if no seed is set)
+        )  # this test pass most of the time, but can fail due to randomness
+        # (if no seed is set)
 
         cov_statistic = TensorStatiticWithEstimationError(
             shape=None,
@@ -113,7 +113,35 @@ class TestTensorStatisticWithError(TestTensorStatistic):
         true_error = torch.norm(cov_statistic() - cov).item() ** 2
         self.assertLessEqual(
             true_error, cov_statistic.error() * 2
-        )  # this test pass most of the time, but can fail due to randomness (if no seed is set)
+        )  # this test pass most of the time, but can fail due to randomness
+        # (if no seed is set)
+
+    def test_stop_trace_computation(self):
+        num_batches = 3
+        batch_size = 10
+        total_samples = torch.Size((num_batches * batch_size,))
+        mean = torch.tensor([3.0, 4.0])
+        cov = torch.tensor([[1.0, 0.5], [0.5, 2.0]])
+
+        dist = torch.distributions.MultivariateNormal(mean, covariance_matrix=cov)
+        samples = dist.sample(total_samples)
+        dataloader = DataLoader(
+            TensorDataset(samples), batch_size=batch_size, shuffle=False
+        )
+
+        mean_statistic = TensorStatiticWithEstimationError(
+            shape=None,
+            update_function=lambda x: (x.sum(dim=0), x.size(0)),
+            name="Mean with Error",
+            trace_precision=5.0,
+        )
+
+        self.assertRaises(AssertionError, mean_statistic.error)
+        for i, (batch,) in enumerate(dataloader):
+            mean_statistic.updated = False
+            mean_statistic.update(x=batch)
+
+        self.assertFalse(mean_statistic._compute_trace)
 
 
 if __name__ == "__main__":
