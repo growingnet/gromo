744fd9fb34af5c14abdc1f6209bc301e27a7ec3a
Stella Douka <35050451+stelladk@users.noreply.github.com>
2025-11-28 20:58:57 +0100

    fix: reshape function in case of global average pooling | operations that flatten the output should never be in the post_merge_function

--- src/gromo/modules/conv2d_growing_module.py
+++ src/gromo/modules/conv2d_growing_module.py
@@ -25,6 +25,7 @@ class Conv2dMergeGrowingModule(MergeGrowingModule):
         input_size: int | tuple[int, int],
         next_kernel_size: int | tuple[int, int],
         post_merge_function: torch.nn.Module = torch.nn.Identity(),
+        reshape_function: torch.nn.Module = torch.nn.Identity(),
         previous_modules: list[GrowingModule | MergeGrowingModule] | None = None,
         next_modules: list[GrowingModule | MergeGrowingModule] | None = None,
         allow_growing: bool = False,
@@ -53,6 +54,7 @@ class Conv2dMergeGrowingModule(MergeGrowingModule):
             device=device,
             name=name,
         )
+        self.reshape_function = reshape_function
 
     @property
     def input_volume(self) -> int:
@@ -72,8 +74,9 @@ class Conv2dMergeGrowingModule(MergeGrowingModule):
     def output_volume(self) -> int:
         if self.input_size is not None:
             with torch.no_grad():
-                x = torch.zeros(1, self.in_channels, *self.input_size)
+                x = torch.zeros(1, self.in_channels, *self.input_size, device=self.device)
                 x = self.post_merge_function(x)
+                x = self.reshape_function(x)
                 return prod(x.shape)
         return self.input_volume
 
