33da30e8df5079ab51616b569303428826d0a669
Théo Rudkiewicz <theo.rudkiewicz@inria.fr>
2026-02-18 00:34:41 +0100

    enh: Made the identity noise dttype dependant

--- src/gromo/utils/tools.py
+++ src/gromo/utils/tools.py
@@ -32,7 +32,7 @@ def sqrt_inverse_matrix_semi_positive(
         # Sometimes, due to numerical issues, we get an error:
         # The algorithm failed to converge because the input matrix is
         # ill-conditioned or has too many repeated eigenvalues
-        matrix += 1e-6 * torch.eye(
+        matrix += torch.finfo(matrix.dtype).resolution * torch.eye(
             matrix.shape[0],
             device=matrix.device,
             dtype=matrix.dtype,
