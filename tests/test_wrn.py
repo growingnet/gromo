5ac615337ae927a552d79a59016512cb2cca67bf
ferdinandlouapre <ferdi.louapre@gmail.com>
2026-04-27 15:13:36 +0000

    fixed test_wrn for 10 classes output

--- tests/test_wrn.py
+++ tests/test_wrn.py
@@ -15,6 +15,7 @@ class TestWideResNet(TorchTestCase):
         Compare the number of parameters in our implementation of WRN-28-1-dropout
         with a reference implementation from
         https://github.com/bmsookim/wide-resnet.pytorch.git
+        on CIFAR-10 (3 input channels, 10 output classes).
         """
         model = init_full_resnet_structure(
             number_of_blocks_per_stage=4,
@@ -23,6 +24,7 @@ class TestWideResNet(TorchTestCase):
             small_inputs=True,
             reduction_factor=1,
             inplanes=16,
+            out_features=10,
         )
 
         n_params = sum(parameter.numel() for parameter in model.parameters())
