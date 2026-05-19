rename growra tests and fix Conv2d covariance batch size

src/gromo/modules/conv2d_growing_module.py
@@ -1083,7 +1083,7 @@ class Conv2dGrowingModule(GrowingModule):
         )
         return (
             torch.einsum("iahw,ibhw->ab", desired_activation, desired_activation),
-            self.input.shape[0],
+            desired_activation.shape[0],
         )
 
     # Layer edition